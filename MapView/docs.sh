ea8507eb4a8f349901675fc7d81250ec24bdef96
Justin R. Miller <incanus@codesorcery.net>
2012-07-24 16:22:12 -0700

    basic doc generation script

---
+++ MapView/docs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+appledoc --project-name "MapBox iOS SDK" \
+         --project-company MapBox \
+         --company-id com.mapbox \
+         --output Documentation \
+         --ignore Map/FMDB \
+         --ignore Map/GRMustache \
+         --ignore build \
+         --ignore Documentation \
+         --ignore Map/NSUserDefaults+RouteMe.h \
+         --ignore Map/RMPath.h \
+         --clean-output \
+         .
